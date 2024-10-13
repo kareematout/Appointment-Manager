26c8761cba0ed926db14e8c49fbbfabbb4e51a4c
Kyle <kylearikata@gmail.com>
2024-10-13 12:34:26 -0700

    Added the test cases to the AppointmentTests class

--- src/tests/AppointmentTests.java
+++ src/tests/AppointmentTests.java
@@ -1,11 +1,11 @@
 package tests;
 
 import obj.Appointment;
-
-import org.junit.Test;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.time.LocalDate;
+import static org.junit.Assert.assertEquals;
 
 public class AppointmentTests {
 
@@ -14,12 +14,43 @@ public class AppointmentTests {
     @Before
     public void setup() {
         LocalDate start = LocalDate.of(2024, 10, 10);
-        LocalDate end = LocalDate.of(2024, 10, 20);
+        LocalDate end = LocalDate.of(2024, 10, 12);
         appointment = new Appointment("test", start, end);
-    }
 
+    }
     @Test
     public void testBeforeStartDate() {
-        // Todo: Test on the appointment object with date before the start which occursOn should return false.
+        LocalDate test = LocalDate.of(2024, 10, 9);
+        boolean actual = appointment.occursOn(test);
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testOnStartDate() {
+        LocalDate test = LocalDate.of(2024, 10, 10);
+        boolean actual = appointment.occursOn(test);
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testBetweenStartAndEndDate() {
+        LocalDate test = LocalDate.of(2024, 10, 11);
+        boolean actual = appointment.occursOn(test);
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testOnEndDate() {
+        LocalDate test = LocalDate.of(2024, 10, 12);
+        boolean actual = appointment.occursOn(test);
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testAfterEndDate() {
+        LocalDate test = LocalDate.of(2024, 10, 13);
+        boolean actual = appointment.occursOn(test);
+        boolean expected = false;
+        assertEquals(expected, actual);
     }
 }
