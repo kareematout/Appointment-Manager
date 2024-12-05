9c4db7558880b37cfda30d9565102bfbfa7aeeee
Kareem Atout <kareem.atout@hotmail.com>
2024-12-04 20:18:14 -0800

    Added Javadoc comments to methods in Appointment and AppointmentManager

--- src/main/java/obj/Appointment.java
+++ src/main/java/obj/Appointment.java
@@ -44,22 +44,40 @@ public abstract class Appointment implements Comparable<Appointment> {
     }
 
     /**
-     * Check if the appointment occurs on a given date
+     * Check if the appointment falls on or between the start and end dates
      *
-     * @param date the date that will be checked
-     * @return true or false depending on if the appointment occurs on a given date
+     * @param date the date to check
+     * @return true if the specified date is within the range of the appointment's
+     * start and end dates, false otherwise
      */
     protected boolean inBetween(LocalDate date) {
         return ((date.isAfter(startDate) || date.isEqual(startDate)) && (date.isBefore(endDate) || date.isEqual(endDate)));
     }
 
+    /**
+     * Abstract method to check if the appointment occurs on a specific date
+     *
+     * @param date the date to check
+     * @return true if the appointment occurs on the specified date, false otherwise
+     */
     public abstract boolean occursOn(LocalDate date);
 
+    /**
+     * Convert the appointment details into a string representation
+     *
+     * @return a formatted string containing the appointment's description, start date, and end date
+     */
     @Override
     public String toString() {
         return String.format("Description: %s | Starts: %s | Ends: %s", description, startDate, endDate);
     }
 
+    /**
+     * Check if two appointments are equal based on their string representations
+     *
+     * @param o the object to compare with this appointment
+     * @return true if the two appointments are equal, false otherwise
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -67,11 +85,23 @@ public abstract class Appointment implements Comparable<Appointment> {
         return Objects.equals(this.toString(), that.toString());
     }
 
+    /**
+     * Generate a hash code for the appointment
+     *
+     * @return an integer hash code based on the description, start date, and end date
+     */
     @Override
     public int hashCode() {
         return Objects.hash(description, startDate, endDate);
     }
 
+    /**
+     * Compare this appointment to another appointment for ordering
+     *
+     * @param o the appointment to compare with this appointment
+     * @return a negative integer, zero, or a positive integer as this appointment is less than,
+     * equal to, or greater than the specified appointment
+     */
     @Override
     public int compareTo(Appointment o) {
         if(!startDate.equals(o.startDate)) {
--- src/main/java/obj/AppointmentManager.java
+++ src/main/java/obj/AppointmentManager.java
@@ -13,23 +13,48 @@ public class AppointmentManager {
         appointments = new HashSet<>();
     }
 
+    /**
+     * Add an appointment to the collection
+     *
+     * @param appointment the appointment to be added
+     * @throws IllegalArgumentException if the appointment already exists in the collection
+     */
     public void add(Appointment appointment) {
         if (!appointments.add(appointment)) {
             throw new IllegalArgumentException("Appointment already exists");
         }
     }
 
+    /**
+     * Remove an appointment from the collection
+     *
+     * @param appointment the appointment to be removed
+     * @throws IllegalArgumentException if the appointment does not exist in the collection
+     */
     public void delete(Appointment appointment) {
         if (!appointments.remove(appointment)) {
             throw new IllegalArgumentException("Appointment does not exist");
         }
     }
 
+    /**
+     * Update an existing appointment by replacing it with a new one
+     *
+     * @param current the existing appointment to be replaced
+     * @param newAppointment the new appointment to replace the existing one
+     */
     public void update(Appointment current, Appointment newAppointment) {
         delete(current);
         add(newAppointment);
     }
 
+    /**
+     * Retrieve all appointments occurring on a specific date, ordered by a given comparator
+     *
+     * @param date  the date to filter appointments by (if null, all appointments are included)
+     * @param order the comparator defining the ordering of the returned appointments
+     * @return an array of appointments occurring on the specified date, sorted by the comparator
+     */
     public Appointment[] getAppointmentsOn(LocalDate date, Comparator<Appointment> order) {
         PriorityQueue<Appointment> temp = new PriorityQueue<>(order);
         if (date == null) {
@@ -44,6 +69,11 @@ public class AppointmentManager {
         return temp.toArray(new Appointment[0]);
     }
 
+    /**
+     * Retrieve the entire collection of appointments
+     *
+     * @return a HashSet containing all appointments
+     */
     public HashSet<Appointment> getAppointments() {
         return appointments;
     }
