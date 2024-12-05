d25b166e088eedf930cbb38bfd3c9c915329a801
Kyle <kylearikata@gmail.com>
2024-12-04 23:56:53 -0800

    Updated UI forms

--- src/main/java/gui/AddAppointmentDialog.java
+++ src/main/java/gui/AddAppointmentDialog.java
@@ -6,6 +6,7 @@ import obj.MonthlyAppointment;
 import obj.OnetimeAppointment;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 import java.time.LocalDate;
 
@@ -21,6 +22,9 @@ public class AddAppointmentDialog extends JDialog {
     private JRadioButton optionMonthly;
     private DatePicker startPicker;
     private DatePicker endPicker;
+    private JLabel statusLbl;
+    private JButton verifyStatusBtn;
+    private JLabel request;
     private Appointment appointment;
 
 
@@ -28,6 +32,7 @@ public class AddAppointmentDialog extends JDialog {
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
+        buttonOK.setEnabled(false);
 
         buttonOK.addActionListener(e -> onOK());
 
@@ -43,11 +48,11 @@ public class AddAppointmentDialog extends JDialog {
 
         // call onCancel() on ESCAPE
         contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        verifyStatusBtn.addActionListener(this::VerifyStatus);
     }
 
-    private void onOK() {
-
-
+    // VerifyStatus method is called when the "Verify Status" button is pressed
+    private void VerifyStatus(ActionEvent e) {
         String description = desField.getText();
         LocalDate start = startPicker.getDate();
         LocalDate end = endPicker.getDate();
@@ -59,6 +64,72 @@ public class AddAppointmentDialog extends JDialog {
             appointment = new MonthlyAppointment(description, start, end);
 
         }
+        // Checks to see if the description, start date and end date are filled
+        if ((description.trim().isEmpty()) || (start == null) || (end == null)){
+            statusLbl.setText("Enter Description, Start Date, and End Date");
+            statusLbl.setForeground(Color.RED);
+            buttonOK.setEnabled(false);
+        } else {
+            statusLbl.setText("Ready to insert appointment");
+            statusLbl.setForeground(Color.GREEN);
+            buttonOK.setEnabled(true);
+        }
+
+        if (optionOnetime.isSelected()){
+            appointment = new OnetimeAppointment(description, start);
+            if (end.equals(start)){
+                statusLbl.setText("Ready to insert appointment");
+                statusLbl.setForeground(Color.GREEN);
+                buttonOK.setEnabled(true);
+
+            } else {
+                statusLbl.setText("Start Date should be Equal to End");
+                statusLbl.setForeground(Color.RED);
+                buttonOK.setEnabled(false);
+            }
+        } else if (optionDaily.isSelected()){
+            appointment = new DailyAppointment(description, start, end);
+            if (appointment.occursOn(end) == true) {
+                statusLbl.setText("Ready to insert appointment");
+                statusLbl.setForeground(Color.GREEN);
+                buttonOK.setEnabled(true);
+            } else {
+                statusLbl.setText("End Date should be after Start");
+                statusLbl.setForeground(Color.RED);
+                buttonOK.setEnabled(false);
+            }
+
+
+        } else if (optionMonthly.isSelected()){
+            appointment = new MonthlyAppointment(description, start, end);
+            if (appointment.occursOn(end) == true) {
+                statusLbl.setText("Ready to insert appointment");
+                statusLbl.setForeground(Color.GREEN);
+                buttonOK.setEnabled(true);
+            } else {
+                statusLbl.setText("Enter Correct End Date");
+                statusLbl.setForeground(Color.RED);
+                buttonOK.setEnabled(false);
+            }
+        }
+
+    }
+
+    private void onOK() {
+
+        /*
+        String description = desField.getText();
+        LocalDate start = startPicker.getDate();
+        LocalDate end = endPicker.getDate();
+        if (optionOnetime.isSelected()){
+            appointment = new OnetimeAppointment(description, start);
+        } else if (optionDaily.isSelected()){
+            appointment = new DailyAppointment(description, start, end);
+        } else if (optionMonthly.isSelected()){
+            appointment = new MonthlyAppointment(description, start, end);
+
+        }
+        */
         dispose();
 
 
@@ -73,6 +144,7 @@ public class AddAppointmentDialog extends JDialog {
 
     private void onCancel() {
         // add your code here if necessary
+        appointment = null;
         dispose();
     }
 }
--- src/main/java/gui/MainGui.java
+++ src/main/java/gui/MainGui.java
@@ -5,42 +5,111 @@ import obj.Appointment;
 import obj.AppointmentManager;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Comparator;
 
 public class MainGui {
     private JPanel panel;
     private JRadioButton displayAllBtn;
     private JRadioButton displayAppointmentsBtn;
-    private JComboBox sortBox;
-    private JList displayList;
+    private JComboBox<String> sortBox;
+    private JList<Appointment> displayList;
     private JButton addBtn;
     private JButton deleteBtn;
     private JButton editBtn;
     private DatePicker datePicker;
+    private JLabel ErrorMessageLbl;
+    private JLabel messageLbl;
+    private JButton updateDisplayBtn;
 
     private AppointmentManager manager;
 
     public MainGui() {
         manager = new AppointmentManager();
         addBtn.addActionListener(this::addAppointment);
-        editBtn.addActionListener(this::addAppointment);
+        editBtn.addActionListener(this::editAppointment);
         deleteBtn.addActionListener(this::deleteAppointment);
+        sortBox.addItem("Date");
+        sortBox.addItem("Description");
+
+        //displayAppointmentsBtn.addActionListener(this::UpdateListBox);
+        //displayAllBtn.addActionListener(this::UpdateListBox);
+        //sortBox.addActionListener(this::UpdateListBox);
+        updateDisplayBtn.addActionListener(this::UpdateListBox);
+    }
+
+    private void UpdateListBox(ActionEvent e){
+        String sortBoxValue = (String) sortBox.getSelectedItem();
+        if (displayAllBtn.isSelected() && sortBoxValue.equals("Date")) {
+            //Comparator<Appointment> comparator = Comparator.comparing(Appointment::getStartDate);
+            Appointment [] appointments = manager.getAppointmentsOn(null, Comparator.comparing(Appointment::getStartDate));
+            for (Appointment appointment : appointments) {
+                System.out.println(appointment);
+            }
+            displayList.setListData(appointments);
+            displayList.revalidate();
+            displayList.repaint();
+            ErrorMessageLbl.setText("Displaying All by 'Date'");
+            ErrorMessageLbl.setForeground(Color.GREEN);
+
+        } else if (displayAllBtn.isSelected() && sortBoxValue.equals("Description")){
+            //Comparator<Appointment> comparator = Comparator.comparing(Appointment::getDescription);
+            Appointment [] appointments = manager.getAppointmentsOn(null, Comparator.comparing(Appointment::getDescription));
+            for (Appointment appointment : appointments) {
+                System.out.println(appointment);
+            }
+            displayList.setListData(appointments);
+            displayList.revalidate();
+            displayList.repaint();
+            ErrorMessageLbl.setText("Displaying All by 'Description'");
+            ErrorMessageLbl.setForeground(Color.GREEN);
+
+        } else if (displayAppointmentsBtn.isSelected() && sortBoxValue.equals("Date") && datePicker.getDate() != null){
+            //Comparator<Appointment> comparator = Comparator.comparing(Appointment::getStartDate);
+            Appointment[] appointments = manager.getAppointmentsOn(datePicker.getDate(), Comparator.comparing(Appointment::getStartDate));
+            for (Appointment appointment : appointments) {
+                System.out.println(appointment);
+            }
+            displayList.setListData(appointments);
+            displayList.revalidate();
+            displayList.repaint();
+            ErrorMessageLbl.setText("Displaying by 'Date'");
+            ErrorMessageLbl.setForeground(Color.GREEN);
+        } else if (displayAppointmentsBtn.isSelected() && sortBoxValue.equals("Description") && datePicker.getDate() != null){
+            //Comparator<Appointment> comparator = Comparator.comparing(Appointment::getDescription);
+            Appointment[] appointments = manager.getAppointmentsOn(datePicker.getDate(), Comparator.comparing(Appointment::getDescription));
+            for (Appointment appointment : appointments) {
+                System.out.println(appointment);
+            }
+            displayList.setListData(appointments);
+            displayList.revalidate();
+            displayList.repaint();
+            ErrorMessageLbl.setText("Displaying by 'Description'");
+            ErrorMessageLbl.setForeground(Color.GREEN);
+        } else if (datePicker.getDate() ==  null) {
+            ErrorMessageLbl.setText("Enter a date");
+            ErrorMessageLbl.setForeground(Color.RED);
+        }
     }
 
     private void addAppointment(ActionEvent e) {
         try{
+
             AddAppointmentDialog dialog = new AddAppointmentDialog();
             dialog.pack();
             dialog.setVisible(true);
             Appointment appointment = dialog.getAppointment();
-            if (e.getSource().equals(addBtn)) {
-                manager.add(appointment);
-                Appointment [] appointments = manager.getAppointmentsOn(null, null);
-                displayList.setListData(appointments);
-            }
 
-            else if (e.getSource().equals(editBtn))
-                System.out.println("Edit btn clicked");
+            if (appointment != null) {
+                manager.add(appointment);
+            }
+            Appointment [] appointments = manager.getAppointmentsOn(null, null);
+            displayList.setListData(appointments);
+            ErrorMessageLbl.setText("Successfully Added");
+            ErrorMessageLbl.setForeground(Color.GREEN);
+
 
         } catch (IllegalArgumentException ex) {
             System.out.println(ex.getMessage());
@@ -48,8 +117,54 @@ public class MainGui {
 
     }
 
+    private void editAppointment(ActionEvent e) {
+        try{
+            if (displayList == null) {
+                ErrorMessageLbl.setText("No appointment available");
+                ErrorMessageLbl.setForeground(Color.RED);
+            } else {
+                Appointment appointmentToUpdate = displayList.getSelectedValue();
+                if (appointmentToUpdate != null) {
+                    AddAppointmentDialog dialog = new AddAppointmentDialog();
+                    dialog.pack();
+                    dialog.setVisible(true);
+                    Appointment appointment = dialog.getAppointment();
+
+                    manager.update(appointmentToUpdate, appointment);
+
+                    Appointment[] appointments = manager.getAppointmentsOn(null, null);
+                    displayList.setListData(appointments);
+                    ErrorMessageLbl.setText("Successfully Updated");
+                    ErrorMessageLbl.setForeground(Color.GREEN);
+                    } else {
+                        ErrorMessageLbl.setText("No appointment selected");
+                        ErrorMessageLbl.setForeground(Color.RED);
+                    }
+
+                }
+
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+
     private void deleteAppointment(ActionEvent e){
         System.out.println("Delete btn clicked");
+        Appointment appointmentDelete = displayList.getSelectedValue();
+        if (appointmentDelete != null) {
+            manager.delete(appointmentDelete);
+            Appointment[] appointments = manager.getAppointmentsOn(null, null);
+            displayList.setListData(appointments);
+            ErrorMessageLbl.setText("Successfully Deleted");
+            ErrorMessageLbl.setForeground(Color.GREEN);
+        } else {
+            ErrorMessageLbl.setText("Select an Appointment");
+            ErrorMessageLbl.setForeground(Color.RED);
+        }
+
+
 
     }
 
